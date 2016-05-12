a5df4e4df3e32937fc128b8fb19abfd4e994d1b2
sherweshb <sbajracharya@trioxis.com>
2016-05-12 22:55:39 +1000

    undo removal of query field from js loader in webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
         test: /\.js$/,
         include: CLIENT_DIR,
         loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
       },
       {
         test: /\.less$/,
