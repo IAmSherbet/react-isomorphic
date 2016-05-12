feb44b506892d30c070167eb8065b6d8d4ca3dad
sherweshb <sbajracharya@trioxis.com>
2016-05-12 23:45:26 +1000

    remove old bundle.js. import App into server/index.js to render react component on the server. run webpack again to rebuild new client and server side bundles.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from './generated/app';
 
 const app = express();
 
 app.get ('/', (request, response) => {
-  response.send('Hello world from Express');
+  response.send(ReactDOMServer.renderToString(<App />));
 });
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000'));
